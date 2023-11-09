e2c4f7aca90aeba74e7064b7b07773dfa42eefb7
Aryan <agupt122@uottawa.ca>
2023-11-08 19:46:24 -0500

    added logout button in admin screen

--- app/src/main/java/com/example/eastcyclingclub/ProfileAdminActivity.java
+++ app/src/main/java/com/example/eastcyclingclub/ProfileAdminActivity.java
@@ -4,13 +4,29 @@ package com.example.eastcyclingclub;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileAdminActivity extends AppCompatActivity {
+
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_admin);
+
+        logout = findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProfileAdminActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ProfileAdminActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.profile);
         bottomNavigationView.setOnItemSelectedListener(item -> {
